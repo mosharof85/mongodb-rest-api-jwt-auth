7147643bf2c712bb4b5d44f9a0fb018fc2f0e291
Mosharof Hossain <mosharofsheikh85@gmail.com>
2023-07-22 01:35:14 +0600

    updated cros-1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";

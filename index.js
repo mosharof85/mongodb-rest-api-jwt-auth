222cc52aed1130f4fa749f9b5266d44d8c95d468
Mosharof Hossain <mosharofsheikh85@gmail.com>
2023-07-22 14:51:56 +0600

    update cors

--- index.js
+++ index.js
@@ -20,16 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(cors());
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+  })
+);
 
 app.use("/api/users", userRoutes);
 
